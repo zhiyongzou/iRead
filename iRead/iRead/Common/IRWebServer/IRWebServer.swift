b01cd4c750f5b732ee76710a424dc426d5b7d345
zzyong <scauzouzhiyong@163.com>
2020-11-17 21:34:58 +0800

    Init IRWebServer and add Search Notes

---
+++ iRead/iRead/Common/IRWebServer/IRWebServer.swift
@@ -0,0 +1,14 @@
+//
+//  IRWebServer.swift
+//  iRead
+//
+//  Created by zzyong on 2020/11/17.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+// https://www.hangge.com/blog/cache/detail_1555.html#
+
+import UIKit
+
+class IRWebServer: NSObject {
+
+}

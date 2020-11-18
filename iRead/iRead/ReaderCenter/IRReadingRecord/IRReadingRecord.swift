4c9ba828feb668f6d2a13febeea266ae40f8b14c
zzyong <scauzouzhiyong@163.com>
2020-11-18 16:26:42 +0800

    IRReadingRecord init

---
+++ iRead/iRead/ReaderCenter/IRReadingRecord/IRReadingRecord.swift
@@ -0,0 +1,13 @@
+//
+//  IRReadingRecord.swift
+//  iRead
+//
+//  Created by zzyong on 2020/11/18.
+//  Copyright © 2020 zzyong. All rights reserved.
+//
+
+import UIKit
+
+class IRReadingRecord: NSObject {
+
+}
